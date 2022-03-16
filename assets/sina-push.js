44d7f79867e9ff76eed27e1b368b069c7d22ed7a
SinaVahidi <94200879+SinaVahidi@users.noreply.github.com>
2022-03-16 11:36:48 -0400

    push

--- assets/sina-push.js
+++ assets/sina-push.js
@@ -91,6 +91,26 @@ console.log(discountBar);
 
 
 $(function() {
+    // grant redesign
+    var $timeline_block = $('.cd-timeline-block');
+
+    //hide timeline blocks which are outside the viewport
+    $timeline_block.each(function(){
+      if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+        $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+      }
+    });
+
+    //on scolling, show/animate timeline blocks when enter the viewport
+    $(window).on('scroll', function(){
+      $timeline_block.each(function(){
+        if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+          $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+        }
+      });
+    });
+    // grant redesign end
+
     $(".coupon-btn" ).click(function() {
       $( ".coupon-area-expand" ).show();
       $( "#coupon-msg" ).text("Please enter your code");
@@ -148,5 +168,4 @@ $(function() {
       $('#yotpo_testimonials_btn').show();
 	});   
 });
-
 
