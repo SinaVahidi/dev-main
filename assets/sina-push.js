854d0f6a538bcdff8133bc3aeebac9edc9efc546
SinaVahidi <94200879+SinaVahidi@users.noreply.github.com>
2022-03-18 10:53:26 -0400

    push

--- assets/sina-push.js
+++ assets/sina-push.js
@@ -85,52 +85,7 @@ ingredientInfoToggle.forEach(element => {
   })
 })
 const discountBar = document.querySelector('.needsclick .kl-teaser-Vjzwx2');
-console.log(discountBar);
 
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
-
-(function () {
-  "use strict";
-
-  // define variables
-  var items = document.querySelectorAll(".timeline li");
-
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
-      }
-    }
-  }
-
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
-})();
 
 $(function() {
     $(".coupon-btn" ).click(function() {
@@ -207,7 +162,7 @@ function popupOpenClose(popup) {
 	
 	/* Open popup */
 	$(popup).show();
-
+  console.log('hello');
 	/* Close popup if user clicks on background */
 	$(popup).click(function(e) {
 		if ( e.target == this ) {
@@ -226,3 +181,14 @@ function popupOpenClose(popup) {
 	});
 }
 
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
